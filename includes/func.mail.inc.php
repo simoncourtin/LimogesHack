d31fa799e0b25bf68b69da873f9a53355d2e5d50
simoncourtin <courtin.simon@gmail.com>
2014-10-05 21:32:07 +0200

    Ajout PHPMailer et mail non spam

---
+++ includes/func.mail.inc.php
@@ -0,0 +1,27 @@
+<?php
+require_once('phpmailer/class.phpmailer.php');
+require_once('phpmailer/class.smtp.php');
+
+function smtpMailer($to, $from, $from_name, $subject, $body) {
+	$mail = new PHPMailer();  // Cree un nouvel objet PHPMailer
+	$mail->CharSet = "UTF-8";
+	$mail->IsSMTP(); // active SMTP
+	$mail->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+	$mail->SMTPAuth = true;  // Authentification SMTP active
+	$mail->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 465;
+	$mail->Username = GMailUSER;
+	$mail->Password = GMailPWD;
+	$mail->IsHTML(true);
+	$mail->SetFrom($from, $from_name);
+	$mail->Subject = $subject;
+	$mail->Body = $body;
+	$mail->AddAddress($to);
+	if(!$mail->Send()) {
+		return 'Mail error: '.$mail->ErrorInfo;
+	} else {
+		return true;
+	}
+}
+?>
